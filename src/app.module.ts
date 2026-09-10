a8a179c6c801cbedfe9aef17e0694e08b5371d7c
bekmurzaernisov- <bekmurzaernisov@gmail.com>
2026-09-10 20:05:36 +0600

    TASK1: Настроил typerom подключил базу данных Postgresql и создал базавую структуру папок

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { EventsModule } from './modules/events/events.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { RegistrationsModule } from './modules/registrations/registrations.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeOrmConfig } from './config/db.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('database')!
+    }),
+    UsersModule,
+    AuthModule, 
+    EventsModule, 
+    CategoriesModule, 
+    RegistrationsModule],
   controllers: [AppController],
   providers: [],
 })
---
+++ src/config/db.config.ts
@@ -0,0 +1,16 @@
+import { registerAs } from "@nestjs/config";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const typeOrmConfig = registerAs(
+  ('database'),
+  (): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || '12345',
+    database: process.env.DB_NAME || 'eventhub',
+    autoLoadEntities: true,
+    synchronize: true,
+  })
+)
