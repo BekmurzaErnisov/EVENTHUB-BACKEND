3c6984f8673db336495a82c079e54f934f3515cc
Pi <picapfo@gmail.com>
2026-09-11 16:51:03 +0600

    TASK-6: сущность, DTO и CRUD для категорий мероприятий

---
+++ src/modules/categories/categories.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entities';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepo: Repository<Category>,
+  ) {}
+
+  create(dto: CreateCategoryDto) {
+    const category = this.categoryRepo.create(dto);
+    return this.categoryRepo.save(category);
+  }
+
+  findAll() {
+    return this.categoryRepo.find();
+  }
+
+  async findOne(id: string) {
+    const category = await this.categoryRepo.findOneBy({ id });
+    if (!category) throw new NotFoundException('Категория не найдена');
+    return category;
+  }
+
+  async update(id: string, dto: UpdateCategoryDto) {
+    const category = await this.findOne(id);
+    Object.assign(category, dto);
+    return this.categoryRepo.save(category);
+  }
+
+  async remove(id: string) {
+    const category = await this.findOne(id);
+    return this.categoryRepo.remove(category);
+  }
+}
