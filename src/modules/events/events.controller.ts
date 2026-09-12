dafcfc730677ed251d3dfbf5c47592336727db1f
Pi <picapfo@gmail.com>
2026-09-12 15:35:11 +0600

    feat: add protected event creation

--- src/modules/events/events.controller.ts
+++ src/modules/events/events.controller.ts
@@ -1,4 +1,25 @@
-import { Controller  } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtAuthGuard } from '../auth/auth.module';
+import { CreateEventDto } from './dto/create-event.dto';
+import { EventsService } from './events.service';
+
+interface AuthenticatedRequest extends Request {
+  user: {
+    id: string;
+  };
+}
+
 @Controller('events')
 export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  create(
+    @Body() createEventDto: CreateEventDto,
+    @Req() request: AuthenticatedRequest,
+  ) {
+    return this.eventsService.create(createEventDto, request.user.id);
+  }
 }
