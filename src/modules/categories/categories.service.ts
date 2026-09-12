6b2231854826a668364fc39453cb00ba7b304040
Pi <picapfo@gmail.com>
2026-09-12 13:11:15 +0600

    feat: add categories module and seeding

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,6 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
 
 @Injectable()
-export class CategoriesService {
-  
+export class CategoriesService implements OnModuleInit {
+  private readonly initialCategories = [
+    'Конференции',
+    'Концерты',
+    'Спорт',
+    'Образование',
+    'Другое',
+  ];
+
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoriesRepository: Repository<Category>,
+  ) {}
+
+  async onModuleInit(): Promise<void> {
+    for (const name of this.initialCategories) {
+      const category = await this.categoriesRepository.findOneBy({ name });
+
+      if (!category) {
+        await this.categoriesRepository.save({ name });
+      }
+    }
+  }
+
+  findAll(): Promise<Category[]> {
+    return this.categoriesRepository.find({ order: { name: 'ASC' } });
+  }
 }
