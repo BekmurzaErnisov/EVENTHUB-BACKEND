5fd34d82ce42bd7f8294ddce808a348e1a0eeae3
saniyaK900 <saniya.karajieva59@gmail.com>
2026-09-14 13:07:23 +0600

    TASK-30:docs: add Swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('EventHub API')
+  .setDescription('API для сервиса мероприятий EventHub')
+  .setVersion('1.0')
+  .build();
+
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
 
   app.enableCors()
 
